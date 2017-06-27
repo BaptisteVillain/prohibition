7a40713897a48bf79dc3b777fbbb750c223bc63c
BaptisteVillain <BVillain92@gmail.com>
2017-06-27 22:29:52 +0200

    :sparkles: timeline !

--- src/js/Class/Framework.js
+++ src/js/Class/Framework.js
@@ -8,19 +8,28 @@ class Framework{
     this.buttons_prev     = document.querySelectorAll('.prev')
     this.sections         = container.querySelectorAll('.container--section, .container--story--row-civil .container--story--section')
     this.button_scroll    = document.querySelector('.fixed-vertical-control')
+    
     this.current_index    = 0
-    this.current_chapter  = 0    
     this.previous_index   = undefined
+    
+    this.current_chapter  = 0
+
+    this.current_section  = 0
+    this.previous_section = 0
+
     this.chapters         = document.querySelectorAll('.timeline--part')
     this.chapter_prev     = document.querySelectorAll('.chapter-prev')
     this.chapter_next     = document.querySelectorAll('.chapter-next')
-    // this.section_next     = document.querySelectorAll('.section-next')
+    this.timeline         = document.querySelector('.timeline')
+    this.timelines_fill   = document.querySelectorAll('.timeline--container--seek-bar-fill')
+    this.buttons_sections = document.querySelectorAll('.timeline--container--pin')
+    this.previous_button  = undefined
 
     this.buttons_prev.forEach(function(button) {
       button.addEventListener('click', (e) => {
         e.preventDefault()    
         if(this.current_index > 0){
-          this.previous_index = this.current_index        
+          this.previous_index = this.current_index    
           this.current_index -= 1
           this.select()
         }
@@ -34,6 +43,31 @@ class Framework{
           this.previous_index = this.current_index
           this.current_index += 1
           this.select()
+
+          if(this.current_index >= 3 ){
+            this.current_section  = (this.current_index - 3) % 4
+            if(this.previous_section == 3 && this.current_section == 0){
+              this.current_chapter += 1
+              this.timeline.style.transform = 'translateX(-'+ (this.current_chapter * 100) +'vw)'
+
+              this.buttons_sections.forEach(function(element) {
+                element.classList.remove('active')
+              }, this);
+              this.timelines_fill[this.current_chapter].style.transform = 'scaleX('+ (this.buttons_sections[3 * this.current_chapter].getBoundingClientRect().left / window.innerWidth) +')'
+              this.buttons_sections[3 * this.current_chapter].classList.add('active')
+
+            }
+            else{
+              if(this.current_section >= 1 && this.current_section <= 3){           
+                this.buttons_sections.forEach(function(element) {
+                    element.classList.remove('active')
+                }, this);
+                this.timelines_fill[this.current_chapter].style.transform = 'scaleX('+ (this.buttons_sections[3 * this.current_chapter + this.current_section -1].getBoundingClientRect().left / window.innerWidth) +')'
+                this.buttons_sections[3 * this.current_chapter + this.current_section-1].classList.add('active')
+              }
+            }
+            this.previous_section = this.current_section
+          }
         }
       })
     }, this)
@@ -41,15 +75,14 @@ class Framework{
     this.button_scroll.addEventListener('click', (e) => {
       e.preventDefault()
       this.container_story.classList.toggle('scrolled')
-      console.log('scroll')
     })
+
 
     this.chapter_prev.forEach(function(button) {
       button.addEventListener('click', (e) => {
         e.preventDefault()    
-        if(this.current_chapter > 0){
+        if(this.current_chapter >= 0){
           this.previous_chapter = this.current_chapter     
-          this.current_chapter -= 1
           this.current_index = this.current_chapter * 4 + 3
           this.selectChapter()
         }
@@ -60,37 +93,56 @@ class Framework{
       button.addEventListener('click', (e) => {
         e.preventDefault()
         if(this.current_chapter < this.chapters.length - 1){
-          this.previous_chapter = this.current_chapter     
           this.current_chapter += 1
-          this.current_index = this.current_chapter * 4 + 3          
+          this.current_index = this.current_chapter * 4 + 3
           this.selectChapter()
         }
       })
     }, this)
 
+    const that = this
+    this.buttons_sections.forEach(function(button) {
+      button.addEventListener('click', function(e){
+        e.preventDefault()
+        that.current_index = Math.floor(this.dataset.target) + (that.current_chapter * 4) + 3
+        that.previous_section = this.dataset.target
+
+        that.buttons_sections.forEach(function(element) {
+          element.classList.remove('active')
+        }, this);
+
+        this.classList.add('active')
+        that.timelines_fill[that.current_chapter].style.transform = 'scaleX('+ (this.getBoundingClientRect().left / window.innerWidth) +')'
+
+        that.select()
+      })
+    }, this)
+
+
   }
 
   selectChapter(){
-    this.container.style.transform = 'translateX(-'+ (this.current_chapter * 100) +'vw)'
-    this.container.classList.add('hide')
-    framework.container.style.transform = 'translateX(-'+ ((this.current_chapter * 4 + 3) * 100) +'vw)'
+    this.timeline.style.transform = 'translateX(-'+ (this.current_chapter * 100) +'vw)'
+
+    this.buttons_sections.forEach(function(element) {
+          element.classList.remove('active')
+    }, this);
+    this.timelines_fill[this.current_chapter].style.transform = 'scaleX('+ (this.buttons_sections[3 * this.current_chapter].getBoundingClientRect().left / window.innerWidth) +')'
+    this.buttons_sections[3 * this.current_chapter].classList.add('active')
+
+    this.select()
   }
 
   select(){
     this.container.style.transform = 'translateX(-'+ (this.current_index * 100) +'vw)'
-    if(!this.sections[this.current_index].classList.contains('container--story--section')){
+    if(this.sections[this.current_index].classList.contains('hide-fixed')){
       this.fixed.classList.add('hide')
     }
     else{
       this.fixed.classList.remove('hide')      
     }
-
-
-
   }
 
 }
 
 const framework = new Framework(document.querySelector('.container'), document.querySelector('.fixed'))
-
-console.log(framework)
