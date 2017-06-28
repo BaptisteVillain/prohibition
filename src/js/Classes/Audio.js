e2105415ef771cabb4678098d45b1d0b77813e1e
BaptisteVillain <BVillain92@gmail.com>
2017-06-29 00:01:37 +0200

    :rocket: mafia 1-4 section, & audio class

---
+++ src/js/Classes/Audio.js
@@ -0,0 +1,31 @@
+class Music{
+  constructor(){
+    this.audio = new Audio
+    this.buttons_play = document.querySelectorAll('.audio-listen')
+    this.buttons_pause = document.querySelectorAll('.audio-mute')
+
+    const that = this
+    this.buttons_play.forEach(function(button) {
+      button.addEventListener('click', function(){
+        that.audio.src = this.dataset.src
+        that.audio.play()
+        that.buttons_pause.forEach(function(button) {
+          button.classList.remove('hide')
+        },this)
+      })
+    }, this);
+    
+    this.buttons_pause.forEach(function(button) {
+      button.addEventListener('click', () => {
+        this.audio.pause()
+        that.buttons_pause.forEach(function(button) {
+          button.classList.add('hide')
+        },this)
+      })
+    }, this);
+
+  }
+
+}
+
+const listener = new Music()
