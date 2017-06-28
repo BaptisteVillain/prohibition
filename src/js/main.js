54bcbd6502f0686963a736f37fa3cfcbd8531ef3
thibobbs <thibaut.renard@hetic.net>
2017-06-28 18:28:59 +0200

    :rocket: civil-1-3 added

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,5 @@ console.log('mdr')
 
 let test = [[11, 1], [12, 7], [13, 3], [17, 5], [22, 1], [78, 6], [89, 3], [90, 8]]
 
-const graph_maphia = new Graph(document.querySelector(".graph-mafia"), test, 1)
+new Graph(document.querySelector(".graph-civil-1"), test, 2)
+new Graph(document.querySelector(".graph-mafia-1"), test, 1)
