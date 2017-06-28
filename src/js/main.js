d3919711f6bcdc84606526728ce7efaf8ec0ef04
thibobbs <thibaut.renard@hetic.net>
2017-06-28 17:41:14 +0200

    :art: js structured

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
 console.log('mdr')
+
+let test = [[11, 1], [12, 7], [13, 3], [17, 5], [22, 1], [78, 6], [89, 3], [90, 8]]
+
+const graph_maphia = new Graph(document.querySelector(".graph-mafia"), test, 1)
